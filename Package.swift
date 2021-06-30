f70bcfdd2df8d1d06fe68e8cf101bed6b71e2131
Alexandr Honcharenko <alex.devios93@gmail.com>
2021-07-01 00:02:42 +0300

    aaa

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "secondApp",
-            dependencies: []),
-        .testTarget(
-            name: "secondAppTests",
-            dependencies: ["secondApp"]),
+            path: "./secondApp/new"),
     ]
 )
--- Sources/secondApp/secondApp.swift
+++
@@ -1,3 +0,0 @@
-struct secondApp {
-    var text = "Hello, World!"
-}
