0c2dcc278e6cf22ac28c4e4b8490bd0b88a1e416
Alexandr Honcharenko <alex.devios93@gmail.com>
2021-06-30 23:41:33 +0300

    Initial Commit

---
+++ secondApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  secondApp
+//
+//  Created by Alexandr Honcharenko on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
