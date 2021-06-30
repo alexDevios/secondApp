7f2ff7254e7d4c764a80d41a4fd7f803f4a64783
Alexandr Honcharenko <alex.devios93@gmail.com>
2021-06-30 23:44:59 +0300

    add extension

---
+++ secondApp/TestData.swift
@@ -0,0 +1,14 @@
+//
+//  TestData.swift
+//  secondApp
+//
+//  Created by Alexandr Honcharenko on 30.06.2021.
+//
+
+import Foundation
+
+public extension String {
+    func setNew() -> String {
+        return self + " Hello!"
+    }
+}
